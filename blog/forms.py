861b14e0cbcf810ec22c110cd0271b6597571c35
Im-Mohsen <mohsendarabi2003@yahoo.com>
2024-09-26 12:30:02 +0330

    update forms

--- blog/forms.py
+++ blog/forms.py
@@ -12,3 +12,11 @@ class TicketForm(forms.Form):
     email = forms.EmailField(max_length=250, required=True, label="ایمیل")
     phone = forms.CharField(min_length=11, max_length=11, required=True, label="شماره تماس")
     subject = forms.ChoiceField(choices=SUBJECT_CHOICES, label="موضوع")
+
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+        if phone:
+            if not phone.isnumeric():
+                raise forms.ValidationError("شماره تلفن عددی نیست!!")
+            else:
+                return phone
