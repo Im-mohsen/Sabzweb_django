19857aa6911b22c8e1fbf6b756c0e591c3c8e1db
Im-Mohsen <mohsendarabi2003@yahoo.com>
2024-09-30 16:06:31 +0330

    change database to postgresql

--- Djangowebapp/settings.py
+++ Djangowebapp/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'Djangowebapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': 'blog_admin',
+        'PASSWORD': '1051079251Md@',
+        'PORT': '5432',
+        # 'HOST': 'localhost',
     }
 }
 
